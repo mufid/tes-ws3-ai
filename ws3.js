65b82578c59a925d90fb86b19f12db2c9071d6bb
Mufid Afif <mufid@idmail.or.id>
2012-10-05 06:54:20 +0700

    yak, final

--- ws3.js
+++ ws3.js
@@ -1,13 +1,30 @@
 // ===================== Helper =======================
 var _ = require("./underscore.js");
 function l(apa) { console.log(apa); }
+function panik() { l("Paniiikkk!"); }
 
 // ===================== Helper =======================
-function kromosom() {
+
+function kromoprint(apa) {
+	_.each(apa, function(kr) {
+		l(kr.string + " f: " + kr.fitness() + " w: " + kr.beratnya());
+	});
+}
+
+function kromosom(string) {
+	if (string) {
+		var t = this;
+		this.string = [];
+		_.each(string, function(s) {
+			t.string.push(s);
+		});
+	} else {
 	var acak = [0, 0, 0, 0, 0, 0, 0,
 				1, 1, 1, 1, 1, 1, 1];
 	acak = _.shuffle(acak);
+	var t = this;
 	this.string = _.first(acak, 7);
+	}
 }
 
 kromosom.prototype.harga = [300, 150, 100, 80, 100, 150, 200];
@@ -37,20 +54,84 @@ kromosom.prototype.beratnya = function() {
 	return beratTotal;
 };
 kromosom.prototype.valid = function() {
-	if (this.beratnya() > 20) return false; else return true;
+	if (this.beratnya() > 20 && this.beratnya() > 0) return false; else return true;
 }
 
 function selektor(apa, n) {
 	// Pilih kromosom
 	var hasil = [];
-	var jumlah_fitness = _.reduce(apa, function(n, m) { return n+m; });
+	var jumlah_fitness = _.reduce(apa, function(prev, now) {
+			return prev + now.fitness(); 
+		}, 0);
 	_.each(apa, function(individu) {
 		individu.prob = individu.fitness() / jumlah_fitness;
 	});
-	while (hasil.length <= n) {
-		var acakan = _.reduce().
-		if (_.contains(hasil, acakan) == false) _.push(acakan);
+	_.reduce(apa, function(prev, now) {
+		console.log(now, "di", prev, "sampai", now.prob + prev);
+		return prev + now.prob;
+	}, 0);
+	while (hasil.length < n) {
+		var acakan = false;
+		var rand   = Math.random();
+		
+		_.reduce(apa, function(prev, now) {
+			if (acakan === false && now.prob + prev > rand) {
+				acakan = now;
+			} else {
+				return now.prob + prev;
+			}
+		}, 0);
+		// Jangan sampai ada duplikat.
+		l("Random.. got " + rand + " that is " + acakan.string);
+		if (_.contains(hasil, acakan) == false) 
+			hasil.push(acakan);
 	}
+	return hasil;
+}
+
+function crossover(apa) {
+	// Crossover dilakukan di
+	// apa <= 1 : do nothing
+	if (apa.length <= 1) panik();
+
+	// Nah sekarang duplicate dulu
+	var queue = _.first(apa, apa.length);
+	var asli  = _.first(apa, apa.length);
+
+	// Setelah di-duplicate, ambil dua
+	var tukar = function(a, b) {
+		// ambil 3 pertama, tukar
+		var a1 = _.first(a.string, 3),
+		    a2 = _.last (a.string, 4),
+		    b1 = _.first(b.string, 3),
+		    b2 = _.last (b.string, 4),
+		    af = [], bf = [];
+
+		af.push(a1); af.push(b2);
+		bf.push(b1); bf.push(a2);
+
+		var hasil = [];
+		hasil.push(new kromosom(_.flatten(af)));
+		hasil.push(new kromosom(_.flatten(bf)));
+		return hasil;
+	}
+
+	var hasil = [];
+	while (queue.length > 1) {
+		var ambil1 = _.first(queue);
+		queue = _.rest(queue);
+		var ambil2 = _.first(queue);
+		queue = _.rest(queue);
+		hasil = _.union(hasil, tukar(ambil1, ambil2));
+	}
+	// Belum habis?
+	if (queue.length > 0) {
+		hasil = _.union(hasil, tukar(
+									  _.first(queue), 
+									  _.first(_.shuffle(asli))
+									));
+	}
+	// Crossover selesai!
 	return hasil;
 }
 
@@ -59,12 +140,49 @@ function selektor(apa, n) {
 // Mendapatkan enam kromosom
 l("Populasi Awal");
 var populasi_awal = [];
+var seleksi0;
+var crossover0;
 _.range(9999).forEach(function(apa, idx) {
 	var kr = new kromosom();
 	if (populasi_awal.length <= 6 && kr.valid()) populasi_awal.push(kr);
 });
-_.each(populasi_awal, function(apa) { l(apa.string + " : " + apa.fitness()); });
+_.each(populasi_awal, function(apa) { l(apa.string + " f: " + apa.fitness() + " w: " + apa.beratnya()); });
+l("Seleksi 4 ===> ");
+seleksi0 = selektor(populasi_awal, 4);
+l("Seleksi ==>");
+console.log("Dapat", seleksi0);
+l("Hasil crossover seleksi ==> ");
+crossover0 = crossover(seleksi0);
+l(crossover0);
 
-l("Seleksi 4");
+var generasi1 = _.union(_.difference(populasi_awal, seleksi0), crossover0);
+l("GENERASI 1 ==>");
+kromoprint(generasi1);
 
-selektor(populasi_awal);
+// Buang semua yang gak valid!
+generasi1 = _.filter(generasi1, function(ini) {
+	if (ini.valid()) return true;
+	else return false;
+});
+
+l("GENERASI 1 (setelah yang tidak valid dibuang) ==>");
+kromoprint(generasi1);
+
+// Nah sekarang seleksi untuk crossover untuk generasi 2
+var seleksi1 = selektor(generasi1, 4);
+console.log("Dapat", seleksi1);
+l("Crossover ==>");
+var crossover1 = crossover(seleksi1);
+kromoprint(crossover1);
+
+// Satukan yang tidak terkena seleksi dan jadikan
+// mereka bagian dari generasi baru
+var generasi2 = _.union(_.difference(generasi1, seleksi1), crossover1);
+l("GENERASI 2 ===>");
+kromoprint(generasi2);
+generasi2 = _.filter(generasi2, function(ini) {
+	if (ini.valid()) return true;
+	else return false;
+});
+l("GENERASI 2 (REFINED)");
+kromoprint(generasi2);
